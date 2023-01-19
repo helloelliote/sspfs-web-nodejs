4b6856a333bf0ccf8c73d9d73ea6ca36de407c08
helloelliote <hello.elliote@gmail.com>
2023-01-19 22:37:18 +0900

    chore: webpack 설정
    - [node](https://webpack.js.org/configuration/node/)
    - [typescript](https://webpack.js.org/guides/typescript/)

---
+++ webpack.server.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+const path = require("path");
+const _ = require("lodash");
+const nodeExternals = require("webpack-node-externals");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const packageJson = require("./package.json");
+// const packageJsonTools = require('./tools/package.json');
+
+const filterProperties = new Set([
+  "description",
+  "devDependencies",
+  "homepage",
+  "license",
+]);
+
+/**
+ * @see https://webpack.js.org/guides/typescript/
+ * @see https://webpack.js.org/guides/build-performance/
+ * @see https://github.com/TypeStrong/fork-ts-checker-webpack-plugin
+ */
+const config = {
+  context: __dirname, // to automatically find tsconfig.json
+  entry: {
+    app: "./bin/www.ts",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  cache: {
+    type: "filesystem",
+    buildDependencies: {
+      // This makes all dependencies of this file - build dependencies
+      config: [__filename],
+      // By default webpack and loaders are build dependencies
+    },
+  },
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
+  },
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder,
+  resolve: {
+    symlinks: false,
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "views", to: "views" }],
+    }),
+    {
+      apply: (compiler) => {
+        compiler.hooks.compile.tap("package.json", () => {
+          const mergedJson = JSON.stringify(
+            _.merge(packageJson),
+            (k, v) => (v == null || filterProperties.has(k) ? undefined : v),
+            2
+          );
+          fs.writeFileSync(
+            path.resolve(__dirname, "dist", "package.json"),
+            mergedJson
+          );
+        });
+      },
+    },
+  ],
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+};
+
+/**
+ * @see https://webpack.js.org/configuration/mode/#mode-none
+ */
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "eval";
+  }
+
+  if (argv.mode === "production") {
+    config.devtool = false;
+  }
+
+  return config;
+};

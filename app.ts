bcb6c2360213c585f478b075448a0c02bfb5c662
helloelliote <hello.elliote@gmail.com>
2023-01-27 01:10:22 +0900

    feature: 미들웨어 설정
    - https://www.npmjs.com/package/express-session
    - https://www.npmjs.com/package/connect-pg-simple
    - https://www.npmjs.com/package/helmet
    - https://www.npmjs.com/package/cors
    - https://www.npmjs.com/package/csrf

--- app.ts
+++ app.ts
@@ -4,6 +4,12 @@ import express, { NextFunction, Request, Response } from "express";
 import createError from "http-errors";
 import logger from "morgan";
 import path from "path";
+import connect_pg_simple from "connect-pg-simple";
+import expressSession from "express-session";
+import helmet from "helmet";
+import cors from "cors";
+import postgresql from "./middlewares/postgresql";
+import csrf from "./middlewares/csrf";
 import indexRouter from "./routes";
 import apiRouter from "./routes/api";
 
@@ -15,6 +21,19 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+const postgresqlSession = connect_pg_simple(expressSession);
+const session = expressSession({
+  resave: false,
+  // @ts-ignore
+  secret: process.env.SESSION_KEY,
+  store: new postgresqlSession({
+    createTableIfMissing: true,
+    pool: postgresql,
+    schemaName: "private",
+  }),
+});
+
+app.use(helmet());
 /**
  @see Web Development with Node & Express 209쪽
  **/
@@ -23,10 +42,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session);
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(csrf, (req: Request, res: Response, next: NextFunction) => next());
 app.use("/", indexRouter);
-app.use("/api", apiRouter);
+app.use("/api", cors(), apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { _csrfToken: req.csrfToken() });
 });
 
 export default router;

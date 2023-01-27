d47826111f6cbf68ae53fa16cca2d861e389cf5c
helloelliote <hello.elliote@gmail.com>
2023-01-27 10:50:24 +0900

    feature: 서버 정상 종료 설정

--- bin/www.ts
+++ bin/www.ts
@@ -8,19 +8,19 @@ import debug0 from "debug";
 import http from "http";
 import { AddressInfo } from "net";
 
-var debug = debug0("web-nodejs:server");
+const debug = debug0("web-nodejs:server");
 /**
  * Get port from environment and store in Express.
  */
-
-var port: string | number | boolean = normalizePort(process.env.PORT || "3003");
+const port: string | number | boolean = normalizePort(
+  process.env.PORT || "3003"
+);
 app.set("port", port);
 
 /**
  * Create HTTP server.
  */
-
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -29,13 +29,16 @@ var server = http.createServer(app);
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
+server.on("SIGTERM", () => {
+  server.close(() => debug("SIGTERM signal received: HTTP server closed"));
+});
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val: string) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -59,7 +62,7 @@ function onError(error: any) {
     throw error;
   }
 
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -81,7 +84,7 @@ function onError(error: any) {
  */
 
 function onListening() {
-  var addr: AddressInfo | string | null = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
+  const addr: AddressInfo | string | null = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
   debug("Listening on " + bind);
 }

580a9bc82abf527f37aebb35e72c6e05d1c9e265
helloelliote <hello.elliote@gmail.com>
2023-03-01 01:52:14 +0900

    feature: helmet 미들웨어 보안설정 강화
    - 위험한 `"'unsafe-inline'"` 대신 inline script 해시값 직접 추가

--- middlewares/helmet.ts
+++ middlewares/helmet.ts
@@ -3,7 +3,13 @@ import helmet, { HelmetOptions } from "helmet";
 const helmetOptions: HelmetOptions = {
   contentSecurityPolicy: {
     directives: {
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://*.amcharts.com"],
+      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+      "script-src": [
+        "'self'",
+        "https://*.amcharts.com",
+        "'sha256-ePXOiK09wqEBNG7NchdBZ7p/JTRMBfrAStg/+fvqHjE='",
+        "'sha256-8EQ6UM+N1ubHgE9iLJ1+w6yMw4owRyonSYtudyKEbeQ='",
+      ],
     },
   },
   crossOriginEmbedderPolicy: false,

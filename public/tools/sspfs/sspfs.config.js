5e2154896a668bd5fd4a1c3c9d649fddc56104d5
helloelliote <hello.elliote@gmail.com>
2023-03-01 22:50:44 +0900

    feature: 카카오지도 스타일 조정

--- public/tools/sspfs/sspfs.config.js
+++ public/tools/sspfs/sspfs.config.js
@@ -1,9 +1,6 @@
 const sspfsEntries = {
   "js/sspfs/maps.bundle": ["@sspfs/js/index.ts"],
-  "css/sspfs/maps.bundle": [
-    "@sspfs/sass/style.scss",
-    "@sspfs/sass/plugins.scss",
-  ],
+  "css/sspfs/maps.bundle": ["@sspfs/sass/style.scss"],
 };
 
 module.exports = {

916fd4364111e37e50d6e901f2a190da7a296db9
helloelliote <hello.elliote@gmail.com>
2023-03-08 23:17:59 +0900

    feature: 부모 클래스(Layer) 에서 자식 클래스(Vector) 로 속성 이동

--- public/tools/sspfs/js/maps/openlayers/layer/Vector.ts
+++ public/tools/sspfs/js/maps/openlayers/layer/Vector.ts
@@ -1,8 +1,13 @@
-import { GeoJSON } from "ol/format";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+import { FeatureLike } from "ol/Feature";
+import { Coordinate } from "ol/coordinate";
+import { LineString, Point } from "ol/geom";
+import Style, { createDefaultStyle } from "ol/style/Style";
 import Layer from "./Layer";
-import { LayerProperty } from "./Layer.property";
+import { styleProperties, StyleProperty } from "./property.style";
+import { layerProperties, LayerProperty } from "./property.layer";
 
 export default class Vector extends Layer {
   private readonly createLayer: (layer: string) => VectorLayer<VectorSource>;
@@ -17,10 +22,48 @@ export default class Vector extends Layer {
       version: "2.0.0",
     };
     const pathname: string = "geoserver/sspfs47130/ows";
+    const styleCache: Map<string | number, Style[]> = new Map();
+    const styleFunction = (feature: FeatureLike): Style[] => {
+      const id: number | string = feature.getId();
+      if (styleCache.has(id)) {
+        return styleCache.get(id);
+      }
+      const { fac_nam, fac_typ } = feature.getProperties();
+      if (styleProperties.has(fac_typ)) {
+        const styles: Style[] = [];
+        const style: StyleProperty = styleProperties.get(fac_typ);
+        switch (feature.getGeometry().getType()) {
+          case "LineString": {
+            const { image, stroke, text }: StyleProperty = style;
+            //
+            const line = new Style({ stroke, text });
+            text.setText(fac_nam);
+            styles.push(line.clone());
+            //
+            const arrow: Style = new Style({ image });
+            const [[x1, y1], [x2, y2]]: Coordinate[] = (
+              feature.getGeometry() as LineString
+            )
+              .getCoordinates()
+              .reverse();
+            const rotation: number = -1 * Math.atan2(y1 - y2, x1 - x2);
+            arrow.setGeometry(new Point([x1, y1]));
+            arrow.getImage().setRotation(rotation);
+            styles.push(arrow.clone());
+            break;
+          }
+          default:
+            break;
+        }
+        styleCache.set(id, styles);
+        return styles;
+      }
+      return createDefaultStyle(feature, 0);
+    };
 
     this.createLayer = (layer: string): VectorLayer<VectorSource> => {
       const { cql_filter, propertyName, typeName }: LayerProperty =
-        this.properties.get(layer);
+        layerProperties.get(layer);
       const url: string = this.constructUrl(pathname, {
         ...params,
         cql_filter,
@@ -29,12 +72,12 @@ export default class Vector extends Layer {
       });
       return new VectorLayer({
         source: new VectorSource({ format, url }),
-        style: this.styleFunction,
+        style: styleFunction,
       });
     };
   }
 
-  addLayer = (layers: string[]): void => {
+  addLayer(layers: string[]): void {
     super.addLayer(layers, this.createLayer);
-  };
+  }
 }
